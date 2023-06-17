a647e05ab537313ba941fb35c623fa36b0326ca0
maksyuki <maksyuki@126.com>
2023-06-17 10:26:33 +0800

    feat: add apb4 if design

--- common/apb_if.sv
+++ common/apb_if.sv
@@ -1,3 +1,44 @@
-interface apb_if;
-    
+interface apb_if #(
+    parameter APB_ADDR_WIDTH = 32,
+    parameter APB_DATA_WIDTH = 32,
+    parameter TIM_NUM        = 2
+) (
+    input logic pclk,
+    input logic presetn
+);
+
+  logic [ APB_ADDR_WIDTH-1:0] paddr;
+  logic [ APB_DATA_WIDTH-1:0] pwdata;
+  logic                       pwrite;
+  logic                       psel;
+  logic                       penable;
+  logic [ APB_DATA_WIDTH-1:0] prdata;
+  logic                       pready;
+  logic                       pslverr;
+  logic [(TIM_NUM * 2) - 1:0] irq;
+
+  modport slave(
+      input paddr,
+      input pwdata,
+      input pwrite,
+      input psel,
+      input penable,
+      output prdata,
+      output pready,
+      output pslverr,
+      output irq
+  );
+
+  modport master(
+      output paddr,
+      output pwdata,
+      output pwrite,
+      output psel,
+      output penable,
+      input prdata,
+      input pready,
+      input pslverr,
+      input irq
+  );
+
 endinterface
